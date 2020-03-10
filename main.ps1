78bbaa3228bcd6b477cfec0701894573feb6c7d4
Brandon Kurtz <kurtz.brandon@gmail.com>
2020-03-09 22:23:56 -0400

    MountWim appears to now be idempotent

--- main.ps1
+++ main.ps1
@@ -19,7 +19,20 @@ function MountWim {
         LogOutput -message '$discard=$True: Dismounting and remounting...'
         Dismount-WindowsImage -Path $wim_mount -Discard
     }
-    Mount-WindowsImage -ImagePath $boot_wim -Index 1 -Path $wim_mount
+    $freshMount = $True
+    foreach ($image in (Get-WindowsImage -Mounted)) {
+        if ($image.Path -eq $wim_mount) {
+            $freshMount = $False
+            if ($image.MountStatus -ne "Ok") {
+                Mount-WindowsImage -Path $wim_mount -Remount
+                break
+            }
+        }
+    }
+    if($freshMount) {
+        Mount-WindowsImage -ImagePath $boot_wim -Index 1 -Path $wim_mount
+    }
+   # Mount-WindowsImage -ImagePath $boot_wim -Index 1 -Path $wim_mount
 }
 
 function EnsureDirectory {
@@ -54,7 +67,7 @@ function EnsurePEFolder {
         }
         
     } else {
-        LogOutput -message "$folder_path already exists, skipping creation..."
+        LogOutput -message "$folder_path already exists, skipping copype..."
     }
 }
 
@@ -84,20 +97,24 @@ function EnsureDismPackage {
         [string]$PackagePath
     )
 
-    Add-WindowsPackage -Path $mount -PackagePath $PackagePath
+    $addPackage = Add-WindowsPackage -Path $mount -PackagePath $PackagePath
     LogOutput -message "Adding $PackagePath to $mount ..."
 }
 
-
+function AddDismPackages {
+    Param(
+        [Parameter(Mandatory=$True)][string]$base_path,
+        [Parameter(Mandatory=$True)][string]$wim_mount,
+        [Parameter(Mandatory=$True)][string[]]$packages
+    )
+    foreach($package in $packages) {
+        $PackagePath = "$base_path\$package"
+        EnsureDismPackage -mount $wim_mount -PackagePath $PackagePath
+    }
+}
 
 
 function main {
-    # $dism_dir = 'C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\DISM'
-    # $dism_module = $dism_dir
-    # #$dism = "$dism_dir\dism.exe"
-    # # Adding $dism_dir to path allows this to run from Admin Powershell instead of Admin ADK Shell
-    # $env:Path = "$dism_dir;" + $env:Path
-    # Import-Module ($dism_module)
 
     [string[]] $winpe_base_paths = @("C:\WinPE_amd64")
     foreach ($winpe_base_path in $winpe_base_paths) {
@@ -106,21 +123,10 @@ function main {
         
         EnsurePEFolder -folder_path $winpe_base_path
         MountWim -boot_wim $boot_wim -wim_mount $wim_mount -discard $discard
-        
         $shutdown_exe_destination = ($wim_mount + "\Windows\System32\shutdown.exe")
         CopyFileIdempotently -file_path "C:\Windows\System32\shutdown.exe" -full_destination_path $shutdown_exe_destination
-        
-        $WinPE_OCs = ""
-        if ($winpe_base_path -Match "x86") {
-            Set-Variable -Name "WinPE_OCs" -Value "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs"
-        } elseif ($winpe_base_path -Match "amd64") {
-            Set-Variable -Name "WinPE_OCs" -Value "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs"
-        }
 
-        Write-Output 'WinPE_OCs' + $WinPE_OCs
-      
-        
-        [string[]] $packages = @(
+        [string[]] $cabPackages = @(
             "WinPE-WMI.cab",
             "en-us\WinPE-WMI_en-us.cab",
             "WinPE-NetFX.cab",
@@ -133,12 +139,18 @@ function main {
             "en-us\WinPE-StorageWMI_en-us.cab",
             "WinPE-DismCmdlets.cab",
             "en-us\WinPE-DismCmdlets_en-us.cab"
-            )
+        )
         
-        foreach($package in $packages) {
-            $PackagePath = "$WinPE_OCs\$package"
-            EnsureDismPackage -mount $wim_mount -PackagePath $PackagePath
+        #$WinPE_OCs = ""
+        if ($winpe_base_path -Match "x86") {
+            # Set-Variable -Name "WinPE_OCs" -Value "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs"
+            $WinPE_OCs = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\x86\WinPE_OCs"
+        } elseif ($winpe_base_path -Match "amd64") {
+            # Set-Variable -Name "WinPE_OCs" -Value "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs"
+            $WinPE_OCs = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs"
         }
+        # base_path should not have a trailing slash. Packages should not have a prefix slash either.
+        AddDismPackages -base_path $WinPE_OCs -wim_mount $wim_mount -packages $cabPackages
 
         EnsureDirectory -folder_path ($winpe_base_path + "\src")
     }
